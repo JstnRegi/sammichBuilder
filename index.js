e53bebc7d00233fb90681acba0d2ae3fd995a119
Justin Reyes <Justin@Justins-MacBook-Pro.local>
2015-09-04 12:47:43 -0700

    Added server. Port page serves an html file

---
+++ index.js
@@ -0,0 +1,27 @@
+//Requirements
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var db = require('./models');
+var views = path.join(process.cwd(), 'views/');
+
+//port
+var port = 3000;
+
+// CONFIG //
+// serve js & css files
+app.use("/static", express.static("public"));
+app.use("/vendor", express.static("bower_components"));
+// body parser config to accept all datatypes
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req,res) {
+    res.sendFile(path.join(views, 'index.html'))
+});
+
+
+
+app.listen(port, function() {
+   console.log('Listening on port ' + port);
+});
